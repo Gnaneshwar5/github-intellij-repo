dc5ec3315ad5d60a425f4ae5e6ae0ff1323ed91b
Gnaneshwar5 <gnaneshw5@gmail.com>
2025-08-27 17:30:06 +0530

    Commit the latest changes

---
+++ src/main/java/org/example/LargestElement.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 5, 1, 4};
+        int largest = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>largest) {
+                largest = arr[i];
+
+            }
+        }
+            System.out.println("The LargestElement in the array is" +" "+ largest);
+
+        /**
+         Input: [ 7, 2, 5, 1, 4]
+         Output: 7
+         **/
+    }
+}
